a59e2acabed9b4dcea85f22fcf2f7a01e8f78af0
RF511-SD3 <marcosbporto@gmail.com>
2018-03-20 19:59:22 -0300

    heroku

--- heroku-web.js
+++ heroku-web.js
@@ -1,10 +1,4 @@
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
-var logger = morgan('combined');
-var fs = require('fs');
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
-app.use(morgan({'combinedstream': accessLogStream}));
-app.use(gzippo.staticGzip('' + __dirname + '/build'));
-app.listen(process.env.PORT || 5000);
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000);
